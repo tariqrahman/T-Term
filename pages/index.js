066b36a51b6d050c3582d2db336b2421cbbbb371
Tariq Rahman <tariqrahmancollege@gmail.com>
2022-07-27 09:39:15 -0700

    Fix next/Head

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Head from 'next/head';
+import Head from 'next/Head';
 import { Component } from 'react';
 import Terminal from '../components/Terminal';
 import Header from '../components/Header'
