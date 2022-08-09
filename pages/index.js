5e4bbc42509151ba9d4a3d35b2ef892c86331e4f
Tariq Rahman <tariqrahmancollege@gmail.com>
2022-08-09 12:27:57 -0700

    Fix import casing

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Head from 'next/Head';
+import Head from 'next/head';
 import { Component } from 'react';
 import Terminal from '../components/Terminal';
 import Header from '../components/Header'
